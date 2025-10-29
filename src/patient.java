1f22b85b45111561f6c35c9f64ffcf25e9d515d8
BerkeOzdemirr <s230444062@stu.thk.edu.tr>
2025-10-29 14:31:38 +0300

    Fix removePatient method logic

--- src/patient.java
+++ src/patient.java
@@ -1,5 +1,3 @@
-import org.w3c.dom.Node;
-
 import java.util.*;
 //Node class
 public class patient {
@@ -9,12 +7,12 @@ public class patient {
     int age;
     patient next;
     //constructor
-    patient() {
-        id = 0;
-        name = "";
-        severity = 0;
-        age = 0;
-        next = null;
+    patient(int id, String name, int severity, int age) {
+        this.id = id;
+        this.name = name;
+        this.severity = severity;
+        this.age = age;
+        this.next = null;
     }
 }
 class patientList{
@@ -31,49 +29,57 @@ class patientList{
 
     //adding patients to the list
     public void addPatient(int id, String name, int severity, int age){
-        patient pat = new patient();
+        patient pat = new patient(id, name, severity, age);
         //that "if" is checking whether list is empty or not
-        if (head ==null){
-            head = pat;
+        if (head ==null){ //if the list is empty
+            head = pat; //add 1st element on top
         }else{
-            patient temp = head;
-            while (temp.next != null && temp.next.id < pat.id){
-                temp = temp.next;
-            }                      //to be honest that part was hard to understand
-            pat.next = temp.next; // temp.next will show the next element of the next
-            temp.next = pat;    //then current element.next will show temp (new element)
+            patient temp = head; // temp behave like a pointer for that java code
+            while (temp.next != null && temp.next.id < pat.id){ // temp.next.id < pat.id (this part order the patients' id)
+                temp = temp.next; //so tmp moves forward one!!
+            }
+            pat.next = temp.next; // Connect the new node to the following node,
+            temp.next = pat;    // so we can insert it in between without breaking the list.
         }
     }
     //removing patients by id
     public void removePatient(int id) {
-        if (head == null) {
-            System.out.println("Patient list is empty!");
-        }
-        if (head.id == id) {
-            System.out.println("Removing patient with id " +id + head.name);
-            head = head.next;
-        }
         patient temp = head;
-        while (temp.next != null && temp.next.id != id) {
-            temp = temp.next;
-        }
-        //if id does not exist
-        if (temp.next != null) {
-            System.out.println("Patient with id " + id + " has been not found!");
+        if (head ==null){
+            System.out.println("Patient list is empty");
+        } else if (head.id == id) { // if the head itself is the one to remove !!
+            System.out.println("Removing patient with id " + id + " " + head.name);
+            head = head.next; // deleting first element in linked list
+        }else{
+            while (temp.next != null ) { // for removing last one or middle one
+                System.out.println("Removing patient with id " + id + " " + temp.next.name);
+                temp.next = temp.next.next;
+                break;
+            }
         }
     }
     //seeking elements
-    public void findPatient(){
-        if (head ==null){
+    public void findPatient(int id){
+        if(head == null){
             System.out.println("Patient list is empty!");
-        }else {
-             System.out.println("Patient name is: " + head.name + "Patient id is: "+ head.id);
+        }
+        patient temp = head;
+        while (temp.next != null && temp.next.id != id){
+            temp = temp.next;
+        }
+        //final message
+        if (temp.next == null) {
+            System.out.println("Patient with id " + id + " not found!");
+        }else  {
+            System.out.println("Patient found: " + temp.name + " ID: " + temp.id + ", severity: " + temp.severity + ", age: " + temp.age + " ");
         }
     }
     //printing all elements
     public void printList(){
         patient temp = head;
-        while(temp == null && temp != null){
+        while(temp != null){
+            System.out.println("-------------");
+
             System.out.println(temp.id + " " + temp.name + " " + temp.severity + " " + temp.age + " ");
             temp = temp.next;
         }
